3c7a5f369aaad72e8d3a82ac5c86aec5952a508e
ALISTON MATHEUS DOS SANTOS <aliston.msantos@senacsp.edu.br>
2024-04-01 16:33:40 -0300

    Iniciando o terceiro módulo, aprendendo Express

--- 2 - HTTP/app.js
+++ 2 - HTTP/app.js
@@ -1,6 +1,6 @@
 var http = require("http"); //Cria uma variavel
 
 http.createServer(function(requisicao,resposta){//Cria um servidor(cria uma função(traz uma requisição e uma resposta))
-    resposta.end("<h1>Bem vindo ao meu site!</h1><h3>de Aliston Santos</h3>")//Traz uma resposta de retorno para o web
+    resposta.end("<h1>Bem vindo ao meu site!</h1><h6>de Aliston Santos</h6>")//Traz uma resposta de retorno para o web
 }).listen(8181);//listen é a porta em que estámeu localhost
 console.log("Meu servidor está rodando!");//Aqui eu vejo no console que meu servidor está funcionando
