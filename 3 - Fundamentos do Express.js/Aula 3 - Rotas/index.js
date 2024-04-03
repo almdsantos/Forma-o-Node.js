36a801755358886be4c5461e497903663858837a
almdsantos <aliston.m.santos@outlook.com.br>
2024-04-03 19:16:45 -0300

    Notas de aula do terceiro módulo da formação - Rotas

---
+++ 3 - Fundamentos do Express.js/Aula 3 - Rotas/index.js
@@ -0,0 +1,22 @@
+const express = require("express"); // Importando o express
+const app = express(); // Inicializando o express
+
+app.get("/",function(req,res){
+    res.send("<h1>Bem vindo ao meu primeiro exemplo de rotas</h1>")
+});
+
+app.get("/blog",function(req,res){
+    res.send("<h2>Esse é um teste da página de blog</h2>")
+});
+
+app.get("/blog/video",function(req,res){
+    res.send("<h3>Aqui seria a página de video do blog</h3>")
+})
+
+app.listen(4000, function(erro){
+    if (erro){
+        console.log("O servidor não está funcionando");
+    } else {
+        console.log("O servidor está funcionando");
+    }
+})
